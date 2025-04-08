a7680831e5abacd53438e7fa9102fb52ebef3cf6
오태현 <jeffrey@kai-i.com>
2025-04-08 18:19:39 +0900

    Test: 최근 키워드 저장 및 호출 단위테스트 추가
    - 키 중복코드 변수화
    - `getRecentKeywords` 반환타입 변경 `Mono<List<String>>` -> `Flux<String>`

--- event-tracker/event-port/src/main/kotlin/com/jeffreyoh/eventtracker/port/output/RecentSearchRedisPort.kt
+++ event-tracker/event-port/src/main/kotlin/com/jeffreyoh/eventtracker/port/output/RecentSearchRedisPort.kt
@@ -1,10 +1,11 @@
 package com.jeffreyoh.eventtracker.port.output
 
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 interface RecentSearchRedisPort {
 
     fun saveRecentKeyword(userId: Long, keyword: String): Mono<Void>
-    fun getRecentKeywords(userId: Long): Mono<List<String>>
+    fun getRecentKeywords(userId: Long): Flux<String>
 
 }
